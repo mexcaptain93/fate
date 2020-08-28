2281aa0bc233d04393f47f2f53b143b6482ef1b5
Mikhail Golovanov <mexcaptain@gmail.com>
2020-08-28 05:29:25 +0300

    Work on header

--- gulp/tasks/svg.js
+++ gulp/tasks/svg.js
@@ -6,22 +6,22 @@ module.exports = function() {
                     pretty: true
                 }
             }))
-            .pipe($.gp.cheerio({
-                run: function($) {
-                    $('[fill]').removeAttr('fill');
-                    $('[stroke]').removeAttr('stroke');
-                    $('[style]').removeAttr('style');
-                },
-                parserOptions: { xmlMode: true }
-            }))
+            // .pipe($.gp.cheerio({
+            //     run: function($) {
+            //         $('[fill]').removeAttr('fill');
+            //         $('[stroke]').removeAttr('stroke');
+            //         $('[style]').removeAttr('style');
+            //     },
+            //     parserOptions: { xmlMode: true }
+            // }))
             .pipe($.gp.replace('&gt;', '>'))
-            .pipe($.gp.svgSprite({
-                mode: {
-                    symbol: {
-                        sprite: "sprite.svg"
-                    }
-                }
-            }))
+            // .pipe($.gp.svgSprite({
+            //     mode: {
+            //         symbol: {
+            //             sprite: "sprite.svg"
+            //         }
+            //     }
+            // }))
             .pipe($.gulp.dest('./build/static/img/svg/'));
     });
 };
