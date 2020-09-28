60edaf9107ef8803549e86fef698ac58606dec3f
mexcaptain <mexcaptain@gmail.com>
2020-09-28 17:32:22 +0300

    Mobile & tablet headers.

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -1,3 +1,41 @@
 $(document).ready(function () {
     svg4everybody({});
+    header();
 });
+
+
+
+function header() {
+    var togglerMenu = $('.js-mobile-menu-toggler'),
+        header = $('.js-header'),
+        headerMenuClass = 'header_menu-mob-opened',
+        search = $('.js-mobile-search'),
+        togglerSearch = $('.js-mobile-search-toggler'),
+        menu = $('.js-mobile-menu'),
+        level1 = '.mobile-menu__item',
+        level2 = '.submenu';
+
+
+    // Поиск
+    togglerSearch.on('click', function (e) {
+        e.preventDefault();
+        search.toggle();
+        togglerSearch.toggleClass('header__search-btn_active');
+        header.removeClass(headerMenuClass);
+    });
+
+    // Меню
+    togglerMenu.on('click', function (e) {
+        e.preventDefault();
+        header.toggleClass(headerMenuClass);
+        search.hide();
+        $(level2).hide();
+        togglerSearch.removeClass('header__search-btn_active');
+    })
+
+    menu.find(level1).on('click', function (e) {
+        $(level2).hide();
+        $(this).find(level2).show();
+    })
+
+}
